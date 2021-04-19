fdc33e364eaddbe14d19a4c68abb83bec4a97ef0
waquer <waquer@hotmail.com>
2021-04-19 09:35:45 +0800

    fix bug when service not exist

--- wscp/MainWindow.xaml.cs
+++ wscp/MainWindow.xaml.cs
@@ -5,13 +5,21 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace wscp {
+namespace wscp
+{
 
-    public partial class MainWindow : Window {
+    public partial class MainWindow : Window
+    {
 
-        System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
+        private readonly System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
 
-        public MainWindow() {
+        private readonly Brush RedBrush = new SolidColorBrush(Colors.Red);
+        private readonly Brush GreenBrush = new SolidColorBrush(Colors.Green);
+        private readonly Brush GrayBrush = new SolidColorBrush(Colors.Gray);
+
+
+        public MainWindow()
+        {
             InitializeComponent();
             this.Left = SystemParameters.WorkArea.Width - this.Width;
             this.Top = SystemParameters.WorkArea.Height - this.Height;
@@ -19,120 +27,168 @@ namespace wscp {
             notifyIcon.MouseClick += NotifyIcon_Click;
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e) {
-            string[] wss = { "Apache", "Mysql", "Redis" };
-            foreach (string ws in wss) {
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] wss = { "Apache", "MySQL", "Redis" };
+            foreach (string ws in wss)
+            {
                 ServiceController sc = new ServiceController(ws);
                 this.SetStatus(ws, this.CheckRunning(sc));
             }
         }
 
-        private Brush RedBrush = new SolidColorBrush(Colors.Red);
-        private Brush GreenBrush = new SolidColorBrush(Colors.Green);
-
-        private void SetStatus(string scname, bool running) {
+        private void SetStatus(string scname, int status)
+        {
             scname = scname.ToLower();
             Button btn = MainForm.FindName("btn_" + scname) as Button;
             Label sta = MainForm.FindName("sta_" + scname) as Label;
-            if (running) {
-                btn.Content = "Stop";
-                sta.Content = "Running";
-                sta.Foreground = GreenBrush;
-            } else {
-                btn.Content = "Start";
-                sta.Content = "Stopped";
-                sta.Foreground = RedBrush;
+            if (status < 0)
+            {
+                sta.Foreground = GrayBrush;
+                sta.Content = "Disabled";
+                btn.Content = "Disabled";
+                btn.Opacity = 0.5;
+                btn.IsEnabled = false;
             }
-            btn.IsEnabled = true;
-            btn.Opacity = 1;
+            else
+            {
+                if (status > 0)
+                {
+                    sta.Foreground = GreenBrush;
+                    sta.Content = "Running";
+                    btn.Content = "Stop";
+                }
+                else
+                {
+                    sta.Foreground = RedBrush;
+                    sta.Content = "Stopped";
+                    btn.Content = "Start";
+                }
+                btn.Opacity = 1;
+                btn.IsEnabled = true;
+            }
         }
 
-        public void AddLog(string log) {
-            NotifyText.AppendText(log + System.Environment.NewLine);
+        private int CheckRunning(ServiceController sc)
+        {
+            try
+            {
+                return sc.Status != ServiceControllerStatus.Stopped ? 1 : 0;
+            }
+            catch (Exception e)
+            {
+                this.AddLog(e.Message);
+                return -1;
+            }
         }
 
-        private void StartService(ServiceController sc) {
-            if (sc == null || sc.Status == ServiceControllerStatus.Running) {
-                return;
-            }
-            try {
+        private void StartService(ServiceController sc)
+        {
+            try
+            {
+                if (sc.Status == ServiceControllerStatus.Running)
+                {
+                    return;
+                }
                 String scname = sc.ServiceName;
                 this.AddLog("Starting " + scname + "...");
-                new Thread(() => {
+                new Thread(() =>
+                {
                     sc.Start();
                     sc.WaitForStatus(ServiceControllerStatus.Running);
-                    Application.Current.Dispatcher.BeginInvoke(new Action(() => {
-                        this.SetStatus(scname, true);
+                    Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        this.SetStatus(scname, 1);
                         this.AddLog(scname + " is Running");
                     }));
                 }).Start();
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 this.AddLog("ERROR：" + e.Message);
             }
         }
 
-        private void StopService(ServiceController sc) {
-            if (sc == null || sc.Status == ServiceControllerStatus.Stopped) {
-                return;
-            }
-            try {
+        private void StopService(ServiceController sc)
+        {
+            try
+            {
+                if (sc.Status == ServiceControllerStatus.Stopped)
+                {
+                    return;
+                }
                 String scname = sc.ServiceName;
                 this.AddLog("Stopping " + scname + "...");
-                new Thread(() => {
+                new Thread(() =>
+                {
                     sc.Stop();
                     sc.WaitForStatus(ServiceControllerStatus.Stopped);
-                    Application.Current.Dispatcher.BeginInvoke(new Action(() => {
-                        this.SetStatus(scname, false);
+                    Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        this.SetStatus(scname, 0);
                         this.AddLog(scname + " is Stopped");
                     }));
                 }).Start();
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 this.AddLog("ERROR：" + e.Message);
             }
         }
 
-        private bool CheckRunning(ServiceController sc) {
-            if (sc != null) {
-                return sc.Status != ServiceControllerStatus.Stopped;
-            }
-            return false;
+        private void AddLog(string log)
+        {
+            NotifyText.AppendText(log + System.Environment.NewLine);
         }
 
-        private void BtnClick(String scname) {
+        private void BtnClick(String scname)
+        {
             ServiceController sc = new ServiceController(scname);
             Button btn = MainForm.FindName("btn_" + scname.ToLower()) as Button;
             btn.IsEnabled = false;
             btn.Opacity = 0.5;
-            if (this.CheckRunning(sc)) {
+            int status = this.CheckRunning(sc);
+            if (status > 0)
+            {
                 this.StopService(sc);
-            } else {
+            }
+            else if (status == 0)
+            {
                 this.StartService(sc);
             }
         }
 
-        private void Btn_apache_Click(object sender, RoutedEventArgs e) {
+        private void Btn_apache_Click(object sender, RoutedEventArgs e)
+        {
             this.BtnClick("Apache");
         }
 
-        private void Btn_mysql_Click(object sender, RoutedEventArgs e) {
+        private void Btn_mysql_Click(object sender, RoutedEventArgs e)
+        {
             this.BtnClick("Mysql");
         }
 
-        private void Btn_redis_Click(object sender, RoutedEventArgs e) {
+        private void Btn_redis_Click(object sender, RoutedEventArgs e)
+        {
             this.BtnClick("Redis");
         }
 
-        private void MainForm_StateChanged(object sender, EventArgs e) {
-            if (WindowState == WindowState.Minimized) {
+        private void MainForm_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
                 ShowInTaskbar = false;
                 notifyIcon.Visible = true;
-            } else {
+            }
+            else
+            {
                 ShowInTaskbar = true;
                 notifyIcon.Visible = false;
             }
         }
 
-        private void NotifyIcon_Click(object sender, EventArgs e) {
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
             WindowState = WindowState.Normal;
         }
 
