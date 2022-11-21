36285fe53a40a517163d4267a097cb66f62bb9fc
mryork <mryork@sina.com>
2022-11-21 23:59:18 +0800

    add RegUtil

--- wscp/MainWindow.xaml.cs
+++ wscp/MainWindow.xaml.cs
@@ -10,16 +10,16 @@ namespace wscp
 
         private readonly string[] SCNameList = { "Apache", "MySQL", "Redis", "nacos" };
 
-        private readonly NotifyIcon notifyIcon;
+        private readonly NotifyIcon notifyIcon = new NotifyIcon();
 
         public MainWindow()
         {
             InitializeComponent();
-            this.notifyIcon = new NotifyIcon();
-            this.notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
-            this.notifyIcon.MouseClick += NotifyIcon_Click;
             this.Left = SystemParameters.WorkArea.Width - this.Width;
             this.Top = SystemParameters.WorkArea.Height - this.Height;
+            this.notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(System.Windows.Forms.Application.ExecutablePath);
+            this.notifyIcon.MouseClick += NotifyIcon_Click;
+            RegUtil regUtil = new RegUtil();
         }
 
         private void NotifyIcon_Click(object sender, EventArgs e)
---
+++ wscp/RegUtil.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wscp
+{
+    internal class RegUtil
+    {
+
+        private readonly string RegistryKeyPath = @"SOFTWARE\MyLittleTools\WebServerControlPanel";
+
+        public RegUtil()
+        {
+            RegistryKey RegKey = this.GetKey();
+            RegKey.Close();
+        }
+
+        private RegistryKey GetKey()
+        {
+            RegistryView useRegistryView = Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32;
+            RegistryKey hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, useRegistryView);
+            return hklm.CreateSubKey(RegistryKeyPath, true);
+        }
+
+        // 读取注册表
+        public string GetValue(string name)
+        {
+            RegistryKey RegKey = this.GetKey();
+            string value = RegKey.GetValue(name).ToString();
+            RegKey.Close();
+            return value;
+        }
+
+        // 写入注册表
+        public void SaveValue(string name, string value)
+        {
+            RegistryKey RegKey = this.GetKey();
+            RegKey.SetValue(name, value);
+            RegKey.Close();
+        }
+
+        // 删除注册表
+        public void DelValue(string name)
+        {
+            RegistryKey RegKey = this.GetKey();
+            RegKey.DeleteValue(name);
+            RegKey.Close();
+        }
+
+    }
+}
