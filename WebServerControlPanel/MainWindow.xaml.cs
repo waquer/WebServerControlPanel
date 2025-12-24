34bf17fb4efbf40bed229c125bed1f07c3a79f9b
mryork <mryork@sina.com>
2025-12-24 23:06:42 +0800

    ...

--- WebServerControlPanel/MainWindow.xaml.cs
+++ WebServerControlPanel/MainWindow.xaml.cs
@@ -16,16 +16,14 @@ namespace WebServerControlPanel
 
         private readonly System.Windows.Forms.NotifyIcon _notifyIcon = new System.Windows.Forms.NotifyIcon();
 
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
             InitTrayAndPosition();
             InitServiceNameList();
             LoadAllService();
         }
 
-        private void InitTrayAndPosition()
-        {
+        private void InitTrayAndPosition() {
             Left = SystemParameters.WorkArea.Width - Width;
             Top = SystemParameters.WorkArea.Height - Height;
             _notifyIcon.Icon =
@@ -33,68 +31,53 @@ namespace WebServerControlPanel
             _notifyIcon.MouseClick += NotifyIcon_Click;
         }
 
-        private void InitServiceNameList()
-        {
+        private void InitServiceNameList() {
             var scnames = RegUtil.GetScNameList();
-            if (scnames == null || scnames.Length < 1)
-            {
+            if (scnames == null || scnames.Length < 1) {
                 scnames = new[] { "Apache", "MySQL", "Redis" };
                 RegUtil.SaveScNameList(scnames);
             }
 
-            for (int i = 0, len = scnames.Length; i < len; i++)
-            {
-                _scItemList.Add(new ScItem(scnames[i], NotifyText));
+            for (int i = 0, len = scnames.Length; i < len; i++) {
+                _scItemList.Add(new ScItem(scnames[i], AddLog));
             }
 
             ServicesDataGrid.ItemsSource = _scItemList;
         }
 
-        private void LoadAllService()
-        {
-            try
-            {
+        private void LoadAllService() {
+            try {
                 ServiceSelectBox.ItemsSource = ServiceController.GetServices().ToList();
-            }
-            catch (Exception ex)
-            {
-                var log = $"加载服务列表失败: {ex.Message}";
-                NotifyText.AppendText(log + Environment.NewLine);
+            } catch (Exception ex) {
+                AddLog($"加载服务列表失败: {ex.Message}");
             }
         }
 
-        private void MainForm_StateChanged(object sender, EventArgs e)
-        {
+        private void MainForm_StateChanged(object sender, EventArgs e) {
             var iconShow = WindowState == WindowState.Minimized;
             _notifyIcon.Visible = iconShow;
             ShowInTaskbar = !iconShow;
         }
 
-        private void NotifyIcon_Click(object sender, EventArgs e)
-        {
+        private void NotifyIcon_Click(object sender, EventArgs e) {
             WindowState = WindowState.Normal;
         }
 
-        private void UpdateScNameList()
-        {
+        private void UpdateScNameList() {
             var names = _scItemList.Select(item => item.ServiceName).ToList();
             RegUtil.SaveScNameList(names);
         }
 
-        private void ServiceAdd_Click(object sender, RoutedEventArgs e)
-        {
+        private void ServiceAdd_Click(object sender, RoutedEventArgs e) {
             var item = (ServiceController)ServiceSelectBox.SelectedItem;
 
             var name = item.ServiceName;
             if (name.Length <= 0) return;
             var selectedIndex = ServicesDataGrid.SelectedIndex;
-            var scitem = new ScItem(name, NotifyText);
-            if (selectedIndex >= 0)
-            {
+            var scitem = new ScItem(name, AddLog);
+            if (selectedIndex >= 0) {
                 _scItemList.Insert(selectedIndex, scitem);
-            }
-            else
-            {
+            } else {
                 _scItemList.Add(scitem);
             }
 
@@ -102,8 +85,7 @@ namespace WebServerControlPanel
             UpdateScNameList();
         }
 
-        private void ServiceDel_Click(object sender, RoutedEventArgs e)
-        {
+        private void ServiceDel_Click(object sender, RoutedEventArgs e) {
             var selectedIndex = ServicesDataGrid.SelectedIndex;
             if (selectedIndex < 0) return;
 
@@ -115,41 +97,43 @@ namespace WebServerControlPanel
             UpdateScNameList();
         }
 
-        private void ServiceUp_Click(object sender, RoutedEventArgs e)
-        {
+        private void ServiceUp_Click(object sender, RoutedEventArgs e) {
             var selectedIndex = ServicesDataGrid.SelectedIndex;
             if (selectedIndex <= 0) return;
             _scItemList.Move(selectedIndex, selectedIndex - 1);
             UpdateScNameList();
         }
 
-        private void ServiceDown_Click(object sender, RoutedEventArgs e)
-        {
+        private void ServiceDown_Click(object sender, RoutedEventArgs e) {
             var selectedIndex = ServicesDataGrid.SelectedIndex;
             if (selectedIndex >= ServicesDataGrid.Items.Count - 1) return;
             _scItemList.Move(selectedIndex, selectedIndex + 1);
             UpdateScNameList();
         }
 
-        private void ServiceAction_Click(object sender, RoutedEventArgs e)
-        {
+        private void ServiceAction_Click(object sender, RoutedEventArgs e) {
             var button = sender as Button;
             var service = (ScItem)button?.Tag;
-            if (service == null) return;
+            if (service == null) {
+                AddLog($"服务参数错误");
+            } else if (service.IsRunning) {
+                service.Stop();
+            } else if (service.IsStopped) {
+                service.Start();
+            } else {
+                AddLog($"服务状态异常: {service.StatusName}");
+            }
         }
 
-        private void ServicesDataGrid_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
-        {
+        private void ServicesDataGrid_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e) {
             var dep = (DependencyObject)e.OriginalSource;
 
             // 查找DataGridRow
-            while ((dep != null) && !(dep is DataGridRow))
-            {
+            while ((dep != null) && !(dep is DataGridRow)) {
                 dep = VisualTreeHelper.GetParent(dep);
             }
 
-            if (dep is DataGridRow row)
-            {
+            if (dep is DataGridRow row) {
                 // 设置当前行为选中行
                 ServicesDataGrid.SelectedItem = row.DataContext;
                 var selectedIdx = ServicesDataGrid.SelectedIndex;
@@ -158,14 +142,16 @@ namespace WebServerControlPanel
                 // 更新菜单项状态
                 ServiceUpMenu.IsEnabled = selectedIdx > 0;
                 ServiceDownMenu.IsEnabled = selectedIdx < scItemsCount - 1;
-            }
-            else
-            {
+            } else {
                 ServiceUpMenu.IsEnabled = false;
                 ServiceDownMenu.IsEnabled = false;
             }
 
             e.Handled = true;
+        }
+
+        private void AddLog(string log) {
+            NotifyText.AppendText(log + Environment.NewLine);
         }
     }
 }
--- WebServerControlPanel/Utils/ScItem.cs
+++ WebServerControlPanel/Utils/ScItem.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.ServiceProcess;
 using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
 
@@ -12,8 +13,7 @@ namespace WebServerControlPanel.Utils
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
@@ -21,68 +21,79 @@ namespace WebServerControlPanel.Utils
 
         private readonly ServiceController _scInst;
 
-        private readonly TextBox _notifyText;
+        private readonly Action<string> AddLog;
 
-        public ScItem(string scname, TextBox notifyText)
-        {
+        public ScItem(string scname, Action<string> addLog) {
             this._name = scname;
             this._scInst = new ServiceController(scname);
-            this._notifyText = notifyText;
+            this.AddLog = addLog;
         }
 
-        public string ServiceName
-        {
-            get
-            {
-                try
-                {
+        public string ServiceName {
+            get {
+                try {
                     return _scInst.ServiceName;
-                }
-                catch (Exception)
-                {
+                } catch (Exception) {
                     return _name;
                 }
             }
         }
 
-        public string DisplayName
-        {
-            get
-            {
-                try
-                {
+        public string DisplayName {
+            get {
+                try {
                     return _scInst.DisplayName;
-                }
-                catch (Exception)
-                {
+                } catch (Exception) {
                     return _name;
                 }
             }
         }
 
-        public string StatusName
-        {
-            get
-            {
-                try
-                {
-                    return _scInst.Status.ToString();
+        public bool IsRunning {
+            get {
+                try {
+                    return _scInst.Status == ServiceControllerStatus.Running;
+                } catch (Exception) {
+                    return false;
                 }
-                catch (Exception e)
-                {
+            }
+        }
+
+        public bool IsStopped {
+            get {
+                try {
+                    return _scInst.Status == ServiceControllerStatus.Stopped;
+                } catch (Exception) {
+                    return false;
+                }
+            }
+        }
+
+        public bool IsEnabled {
+            get {
+                try {
+                    return _scInst.Status != ServiceControllerStatus.ContinuePending && _scInst.Status != ServiceControllerStatus.PausePending
+                        && _scInst.Status != ServiceControllerStatus.StartPending && _scInst.Status != ServiceControllerStatus.StopPending;
+                } catch (Exception) {
+                    return true;
+                }
+            }
+        }
+
+        public string StatusName {
+            get {
+                try {
+                    return _scInst.Status.ToString();
+                } catch (Exception e) {
                     return e.Message;
                 }
             }
         }
 
-        public string ActionName
-        {
-            get
-            {
-                try
-                {
-                    switch (_scInst.Status)
-                    {
+        public string ActionName {
+            get {
+                try {
+                    switch (_scInst.Status) {
                         case ServiceControllerStatus.Running:
                             return "Stop";
                         case ServiceControllerStatus.Stopped:
@@ -90,63 +101,47 @@ namespace WebServerControlPanel.Utils
                         default:
                             return "Waiting";
                     }
-                }
-                catch (Exception)
-                {
+                } catch (Exception) {
                     return "Invalid";
                 }
             }
         }
 
-        public void Start()
-        {
-            try
-            {
-                if (_scInst.Status == ServiceControllerStatus.Running)
-                {
+        public void Start() {
+            try {
+                if (_scInst.Status == ServiceControllerStatus.Running) {
                     return;
                 }
 
                 AddLog("Starting " + DisplayName + " ...");
-                new Thread(() =>
-                {
+                Application.Current.Dispatcher.Invoke(() => {
                     _scInst.Start();
                     _scInst.WaitForStatus(ServiceControllerStatus.Running);
                     AddLog(DisplayName + " is Running");
-                }).Start();
-            }
-            catch (Exception e)
-            {
+                    OnPropertyChanged(ServiceName);
+                });
+            } catch (Exception e) {
                 AddLog("ERROR：" + e.Message);
             }
         }
 
-        public void Stop()
-        {
-            try
-            {
-                if (_scInst.Status == ServiceControllerStatus.Stopped)
-                {
+        public void Stop() {
+            try {
+                if (_scInst.Status == ServiceControllerStatus.Stopped) {
                     return;
                 }
 
                 AddLog("Stopping " + DisplayName + " ...");
-                new Thread(() =>
-                {
+                Application.Current.Dispatcher.Invoke(() => {
                     _scInst.Stop();
                     _scInst.WaitForStatus(ServiceControllerStatus.Stopped);
                     AddLog(DisplayName + " is Stopped");
-                }).Start();
-            }
-            catch (Exception e)
-            {
+                    OnPropertyChanged(ServiceName);
+                });
+            } catch (Exception e) {
                 AddLog("ERROR：" + e.Message);
             }
         }
 
-        private void AddLog(string log)
-        {
-            Dispatcher.CurrentDispatcher.Invoke(delegate { _notifyText.AppendText(log + Environment.NewLine); });
-        }
     }
 }
