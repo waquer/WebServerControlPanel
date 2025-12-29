aefd63ae22c6e4ab4e589ebce2f0ad13fcee3500
wang <mryork@sina.com>
2025-12-29 14:55:03 +0800

    ...

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,13 +16,15 @@ namespace WebServerControlPanel
 
         private readonly System.Windows.Forms.NotifyIcon _notifyIcon = new System.Windows.Forms.NotifyIcon();
 
-        public MainWindow() {
+        public MainWindow()
+        {
             InitializeComponent();
             InitTrayAndPosition();
             InitServiceNameList();
         }
 
-        private void InitTrayAndPosition() {
+        private void InitTrayAndPosition()
+        {
             Left = SystemParameters.WorkArea.Width - Width;
             Top = SystemParameters.WorkArea.Height - Height;
             _notifyIcon.Icon =
@@ -30,58 +32,66 @@ namespace WebServerControlPanel
             _notifyIcon.MouseClick += NotifyIcon_Click;
         }
 
-        private void InitServiceNameList() {
+        private void InitServiceNameList()
+        {
             var scnames = RegUtil.GetScNameList();
-            if (scnames == null || scnames.Length < 1) {
+            if (scnames == null || scnames.Length < 1)
+            {
                 scnames = new[] { "Redis" };
                 RegUtil.SaveScNameList(scnames);
             }
 
-            for (int i = 0, len = scnames.Length; i < len; i++) {
+            for (int i = 0, len = scnames.Length; i < len; i++)
+            {
                 _scItemList.Add(new ScItem(scnames[i], AddLog));
             }
 
             ServicesDataGrid.ItemsSource = _scItemList;
         }
 
-        private void MainForm_StateChanged(object sender, EventArgs e) {
+        private void MainForm_StateChanged(object sender, EventArgs e)
+        {
             var iconShow = WindowState == WindowState.Minimized;
             _notifyIcon.Visible = iconShow;
             ShowInTaskbar = !iconShow;
         }
 
-        private void NotifyIcon_Click(object sender, EventArgs e) {
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
             WindowState = WindowState.Normal;
         }
 
-        private void UpdateScNameList() {
+        private void UpdateScNameList()
+        {
             var names = _scItemList.Select(item => item.ServiceName).ToList();
             RegUtil.SaveScNameList(names);
         }
 
-        private void ServiceAll_Click(object sender, RoutedEventArgs e) {
-            var serviceWindow = new ServiceWindow();
-            serviceWindow.ShowDialog();
-        }
-
-        private void ServiceAdd_Click(object sender, RoutedEventArgs e) {
-            var item = (ServiceController)ServiceSelectBox.SelectedItem;
-
-            var name = item.ServiceName;
+        private void AddService(string name)
+        {
             if (name.Length <= 0) return;
             var selectedIndex = ServicesDataGrid.SelectedIndex;
             var scitem = new ScItem(name, AddLog);
-            if (selectedIndex >= 0) {
+            if (selectedIndex >= 0)
+            {
                 _scItemList.Insert(selectedIndex, scitem);
-            } else {
+            }
+            else
+            {
                 _scItemList.Add(scitem);
             }
 
-            ServiceSelectBox.Text = null;
             UpdateScNameList();
         }
 
-        private void ServiceDel_Click(object sender, RoutedEventArgs e) {
+        private void ServiceAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var serviceWindow = new ServiceWindow(AddService);
+            serviceWindow.ShowDialog();
+        }
+
+        private void ServiceDel_Click(object sender, RoutedEventArgs e)
+        {
             var selectedIndex = ServicesDataGrid.SelectedIndex;
             if (selectedIndex < 0) return;
 
@@ -93,43 +103,56 @@ namespace WebServerControlPanel
             UpdateScNameList();
         }
 
-        private void ServiceUp_Click(object sender, RoutedEventArgs e) {
+        private void ServiceUp_Click(object sender, RoutedEventArgs e)
+        {
             var selectedIndex = ServicesDataGrid.SelectedIndex;
             if (selectedIndex <= 0) return;
             _scItemList.Move(selectedIndex, selectedIndex - 1);
             UpdateScNameList();
         }
 
-        private void ServiceDown_Click(object sender, RoutedEventArgs e) {
+        private void ServiceDown_Click(object sender, RoutedEventArgs e)
+        {
             var selectedIndex = ServicesDataGrid.SelectedIndex;
             if (selectedIndex >= ServicesDataGrid.Items.Count - 1) return;
             _scItemList.Move(selectedIndex, selectedIndex + 1);
             UpdateScNameList();
         }
 
-        private void ServiceAction_Click(object sender, RoutedEventArgs e) {
+        private void ServiceAction_Click(object sender, RoutedEventArgs e)
+        {
             var button = sender as Button;
             var service = (ScItem)button?.Tag;
-            if (service == null) {
+            if (service == null)
+            {
                 AddLog("服务参数错误");
-            } else if (service.IsRunning) {
+            }
+            else if (service.IsRunning)
+            {
                 service.Stop();
-            } else if (service.IsStopped) {
+            }
+            else if (service.IsStopped)
+            {
                 service.Start();
-            } else {
+            }
+            else
+            {
                 AddLog($"服务状态异常: {service.StatusName}");
             }
         }
 
-        private void ServicesDataGrid_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e) {
+        private void ServicesDataGrid_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
             var dep = (DependencyObject)e.OriginalSource;
 
             // 查找DataGridRow
-            while ((dep != null) && !(dep is DataGridRow)) {
+            while ((dep != null) && !(dep is DataGridRow))
+            {
                 dep = VisualTreeHelper.GetParent(dep);
             }
 
-            if (dep is DataGridRow row) {
+            if (dep is DataGridRow row)
+            {
                 // 设置当前行为选中行
                 ServicesDataGrid.SelectedItem = row.DataContext;
                 var selectedIdx = ServicesDataGrid.SelectedIndex;
@@ -138,7 +161,9 @@ namespace WebServerControlPanel
                 // 更新菜单项状态
                 ServiceUpMenu.IsEnabled = selectedIdx > 0;
                 ServiceDownMenu.IsEnabled = selectedIdx < scItemsCount - 1;
-            } else {
+            }
+            else
+            {
                 ServiceUpMenu.IsEnabled = false;
                 ServiceDownMenu.IsEnabled = false;
             }
@@ -146,7 +171,8 @@ namespace WebServerControlPanel
             e.Handled = true;
         }
 
-        private void AddLog(string log) {
+        private void AddLog(string log)
+        {
             NotifyText.AppendText(log + Environment.NewLine);
         }
     }
--- ServiceWindow.xaml.cs
+++ ServiceWindow.xaml.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Linq;
 using System.ServiceProcess;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WebServerControlPanel
 {
     public partial class ServiceWindow
     {
-        public ServiceWindow()
+        
+        private readonly Action<string> _addService;
+        
+        public ServiceWindow(Action<string> addService)
         {
+            _addService = addService;
             InitializeComponent();
             try
             {
@@ -17,6 +23,18 @@ namespace WebServerControlPanel
             {
                 // 忽略异常
             }
+        }
+        
+        private void AddService_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var service = (ServiceController)button?.Tag;
+            var name = service?.ServiceName;
+            if (name != null)
+            {
+                _addService(name);
+            }
+            Close();
         }
     }
 }
